6c002264058461bcfef6f4c2fbcbd24f2a3bb5b8
dokyung-kim <dkkim0122@gmail.com>
2025-02-02 16:08:57 +0900

    build: rollup 설정
    * terser 사용하여 번들 코드 minify

---
+++ dist/types/index.d.ts
@@ -0,0 +1,4 @@
+import { AudioRecorder } from './types';
+declare function createAudioRecorder(): AudioRecorder;
+export { createAudioRecorder };
+export type { AudioRecorderOptions, AudioRecorderStates, AudioRecorder, } from './types';
---
+++ dist/types/worklet/recorderWorkletProcessor.d.ts
@@ -0,0 +1,1 @@
+export declare const RecorderWokletProcessorSrc: string;
